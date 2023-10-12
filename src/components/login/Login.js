99f817a551ced8e6909ab82477116756c888170d
Lucic15 <lucicluka31@gmail.com>
2023-10-13 00:28:39 +0200

    Nothing Special

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,10 +1,20 @@
-
-
 import "./Login.css";
+import background from "../../images/new/background.jpeg";
+import logo from "../../images/new/logo.png";
 
+
+
+const inputField = new MDCTextField(document.querySelector('.mdc-text-field'));
 const LogIn = () => {
-
-
-  return 
+  return (
+    <div className="main">
+      <div className="img"></div>
+      <div className="title">
+        <h1 className="title-welcome">Welcome</h1>
+        <img src={logo}></img>
+      </div>
+      <TextField label="Username" variant="outlined" />
+    </div>
+  );
 };
 export default LogIn;
