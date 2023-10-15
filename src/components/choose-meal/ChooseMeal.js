452a08edaec19851e0b4711c5957fa5ba3b96852
Lucic15 <lucicluka31@gmail.com>
2023-10-15 19:17:14 +0200

    structure for chooseMeal

--- src/components/choose-meal/ChooseMeal.js
+++ src/components/choose-meal/ChooseMeal.js
@@ -1,47 +1,25 @@
 import "./ChooseMeal.css";
-import "../../style/global.css";
-import slika01 from "../../images/Ellipse 1.svg";
-import slika02 from "../../images/logo.png";
-import slika03 from "../../images/icons/shopping_bag.svg";
+import "../../style/global.css"
+import profileIcon from "../../images/Ellipse 1.svg";
+
+
 
 const ChooseMeal = () => {
   return (
-    <>
-      <div className="choose-meal-head-content">
-        <div className="choose-meal-head-content-without-text">
-          <div className="choose-meal-up-part">
-            <div className="choose-meal-up-section">
-              <div className="choose-meal-topbar">
-                <img src={slika01} alt="img"></img>
-                <div className="choose-meal-other-part-of-topbar">
-                  <img
-                    src={slika02}
-                    className="choose-meal-size-logo-of-company"
-                    alt="img"
-                  ></img>
-                  <img
-                    src={slika03}
-                    className="choose-meal-bag-icon"
-                    alt="img"
-                  ></img>
-                </div>
-              </div>
-              <div className="choose-meal-search-input">
-                <div className="choose-meal-search-input-more-detailed">
-                  <div className="choose-meal-more-detailed">
-                  <input type="text" className="choose-meal-search-input" placeholder="Search"></input></div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="choose-meal-down-part"></div>
-        </div>
-        <div className="choose-meal-head-content-text">
-          <div className="choose-meal-head-content-text">Recommended</div>
-        </div>
-      </div>
-      <div className="choose-meal-main-content"></div>
-    </>
+    <div className="main-choosemeal">
+      <section className="top-choosemeal">
+        <div className="user-bar-choosemeal"> <img
+            src={profileIcon}
+            alt="logo"
+            className="image-topbar-home-profile"
+          ></img></div>
+        <div className="search-choosemeal"></div>
+        <div className="categories-choosemeal"></div>
+      </section>
+      <section className="meals-list-choosemeal">
+        <div className="meals-title-choosemeal"></div>
+      </section>
+    </div>
   );
 };
 
