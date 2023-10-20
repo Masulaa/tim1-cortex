d51f7fcfb84b2ec96a606f0124f8a3e04947c100
Luka <lukamasulovic2@gmail.com>
2023-10-20 11:19:52 +0200

    full finished settings

--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -1,7 +1,36 @@
 import "./Settings.css";
-import { ArrowBackIosNew } from "@mui/icons-material";
 
+import { useState } from "react";
+
+import { ArrowBackIosNew } from "@mui/icons-material";
+import active from "../../images/icons/active.svg";
+import inactive from "../../images/icons/inactive.svg";
 const Settings = () => {
+  const [days, setDays] = useState({
+    Monday: true,
+    Tuesday: true,
+    Wednesday: true,
+    Thursday: true,
+    Friday: true,
+    Saturday: false,
+    Sunday: false,
+  });
+
+  const [dayText, setDayText] = useState({
+    Monday: 'active',
+    Tuesday: 'active',
+    Wednesday: 'active',
+    Thursday: 'active',
+    Friday: 'active',
+    Saturday: 'inactive',
+    Sunday: 'inactive',
+  });
+
+  const toggleDay = (day) => {
+    setDays({ ...days, [day]: !days[day] });
+    setDayText({ ...dayText, [day]: days[day] ? 'inactive' : 'active' });
+  };
+
   return (
     <div className="main-settings">
       <div className="settings-head">
@@ -10,10 +39,27 @@ const Settings = () => {
       </div>
       <div className="settigs-content">
         <div className="settings-mini-title-and-description">
-            <p className="settings-mini-title">Meal days</p>
-            <p className="settings-description">Select the days for which you want meals for your business.</p>
+          <p className="settings-mini-title">Meal days</p>
+          <p className="settings-description">
+            Select the days for which you want meals for your business.
+          </p>
         </div>
-      </div>
+        <div className="settings-days">
+          {Object.keys(days).map((day) => (
+            <div className="settings-day" key={day}>
+              <img
+                src={days[day] ? active : inactive}
+                className="settings-active-button"
+                alt="radio-button"
+                onClick={() => toggleDay(day)}
+              />
+              <p className={`settings-day-text-${dayText[day]}`}>{day}</p>
+            </div>
+          ))}
+        </div>
+        
+      </div><div className="settings-button">
+        <button className="primary-button">DONE</button></div>
     </div>
   );
 };
